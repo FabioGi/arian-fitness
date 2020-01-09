1ae10fa6c36bed9520a592dfea879aa9709e34a5
C.B <cbanzet@gmail.com>
2020-01-09 19:15:48 +0100

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBbMF0-b1KQjTJhTzS5WVOo98XkTFTXSyA",
+        authDomain: "arian-fitness.firebaseapp.com",
+        databaseURL: "https://arian-fitness.firebaseio.com",
+        projectId: "arian-fitness",
+        storageBucket: "arian-fitness.appspot.com",
+        messagingSenderId: "723173069485",
+        appId: "1:723173069485:web:cde9529c9572fbc589ec24",
+        measurementId: "G-RJ2JM92JXD"
+    }
+};
